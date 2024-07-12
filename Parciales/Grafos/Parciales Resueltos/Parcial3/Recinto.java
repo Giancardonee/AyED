966f5fd6376b1fb13b6604984d741dbd556e052b
Gianca <giancardonee@gmail.com>
2024-07-12 01:05:19 -0300

    Add files via upload

---
+++ Parciales/Grafos/Parciales Resueltos/Parcial3/Recinto.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialesgrafos.Parcial3;
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class Recinto {
+    private String nombre;
+    private int tiempo;
+    
+    public Recinto(String nombre, int tiempo)
+    {
+        this.nombre = nombre;
+        this.tiempo = tiempo;
+    }
+    
+    public int getTiempo()
+    {
+        return this.tiempo;
+    }
+    
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+}
